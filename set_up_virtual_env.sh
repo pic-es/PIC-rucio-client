87a5158e8b13817a1d071a8314a2568f0fca1f19
BruzzeseA <bruzzese.agustin@gmail.com>
2021-02-15 06:48:43 -0800

    Add files via upload

---
+++ set_up_virtual_env.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# FYI please adress to the following link if 
+# you want to install further dependencies in jupyter.pic
+
+apt install python3-pip
+
+pip install virtualenv
+python3 -m venv rucio
+# virtualenv rucio
+
+source rucio/bin/activate
+
+git clone https://github.com/pic-es/PIC-rucio-client.git
+
+pip install -r PIC-rucio-client/requirements.txt
+
+pip install -Iv rucio-clients==1.23.11
+
+cp Rucio-Clients-Scripts/rucio.cfg rucio/etc
+
+export RUCIO_HOME=`pwd`/rucio/
+
